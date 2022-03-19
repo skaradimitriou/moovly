dd52c6a7667a9fe996b985e9a662bd84dd69d6c9
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2022-03-19 13:12:35 +0200

    Implementation: Login Screen Firebase Auth login

--- app/src/main/java/com/stathis/moovly/features/login/login/LoginViewModel.kt
+++ app/src/main/java/com/stathis/moovly/features/login/login/LoginViewModel.kt
@@ -3,6 +3,7 @@ package com.stathis.moovly.features.login.login
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -10,12 +11,16 @@ import kotlinx.coroutines.launch
 class LoginViewModel : ViewModel() {
 
     val loggedIn = MutableLiveData<Boolean>()
+    private val auth by lazy { FirebaseAuth.getInstance() }
 
     fun login(email : String, pass : String){
         viewModelScope.launch(Dispatchers.IO){
-            val checkCredentials = email == "admin@gmail.com" && pass == "Test1234!"
-            delay(2000)
-            loggedIn.postValue(checkCredentials)
+            auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener {
+                when(it.isSuccessful){
+                    true -> loggedIn.postValue(true)
+                    false -> loggedIn.postValue(false)
+                }
+            }
         }
     }
 }
